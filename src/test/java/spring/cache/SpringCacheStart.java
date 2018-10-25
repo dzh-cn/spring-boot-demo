a5adcbaf0558bbf7aae90a6c11dfe88876eb6b51
董智华 <dongzhihua@jd.com>
2018-10-25 12:44:13 +0800

    spring cache

---
+++ src/test/java/spring/cache/SpringCacheStart.java
@@ -0,0 +1,35 @@
+package spring.cache;
+
+import com.DemoBootApplication;
+import com.springCache.OrderBean;
+import com.springCache.OrderService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * spring cache start
+ *
+ * @author: dongzhihua
+ * @time: 2018/10/25 12:02:01
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DemoBootApplication.class)
+public class SpringCacheStart {
+    private static Logger logger = LoggerFactory.getLogger(SpringCacheStart.class);
+    @Resource
+    OrderService orderService;
+    @Test
+    public void testCache() throws InterruptedException {
+        int i = 1;
+        while (i++ < 20) {
+            OrderBean orderBean = orderService.getOrderBean(1);
+            logger.info("testCache : {}", orderBean);
+        }
+    }
+}
