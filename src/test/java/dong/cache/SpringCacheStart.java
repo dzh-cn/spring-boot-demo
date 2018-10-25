8b10bbdfc52ccebde061b80b9498d46779912178
董智华 <dongzhihua@jd.com>
2018-10-25 13:45:56 +0800

    cache

--- src/test/java/dong/cache/SpringCacheStart.java
+++ src/test/java/dong/cache/SpringCacheStart.java
@@ -6,6 +6,8 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
@@ -22,6 +24,10 @@ public class SpringCacheStart {
     private static Logger logger = LoggerFactory.getLogger(SpringCacheStart.class);
     @Resource
     OrderService orderService;
+
+    @Resource
+    CacheManager cacheManager;
+
     @Test
     public void testCache() throws InterruptedException {
         int i = 1;
@@ -29,5 +35,14 @@ public class SpringCacheStart {
             OrderBean orderBean = orderService.getOrderBean(1);
             logger.info("testCache : {}", orderBean);
         }
+    }
+
+    @Test
+    public void manualCache() {
+    	logger.info("cache ----: {}", cacheManager);
+        Cache cus = cacheManager.getCache("cus");
+        logger.info("cache put before: {}", cus.get("name"));
+        cus.put("name", "value");
+        logger.info("cache put after: {}", cus.get("name").get());
     }
 }
